1276fb518e77b1f8efb32be49231ea1e0d1daafd
Aderox <aderoxgame@gmail.com>
2022-02-05 00:10:56 +0100

    i'll cover it up

--- test_hcsr04.c
+++ test_hcsr04.c
@@ -75,50 +75,32 @@ int main(int argc, char *argv[])
         poke();
 
         
-        int echo, previousEcho, lowHigh, highLow;
-        long startTime, stopTime, difference;
-        float rangeCm;
-        lowHigh = highLow = echo = previousEcho = 0;
-        while(0 == lowHigh || highLow == 0) {
-            previousEcho = echo;
-            echo = gpioRead(GPIO_ECHO);
-            if(0 == lowHigh && 0 == previousEcho && 1 == echo) {
-            lowHigh = 1;
-            startTime = getMicrotime();
-            }
-            if(1 == lowHigh && 1 == previousEcho && 0 == echo) {
-            highLow = 1;
-            stopTime = getMicrotime();
-            }
-        }
-        difference = stopTime - startTime;
-        rangeCm = difference / 58;
-        printf("Start: %ld, stop: %ld, difference: %ld, range: %.2f cm\n", startTime, stopTime, difference, rangeCm);
-        
-
-        /*uint32_t start = 0;
+        uint32_t start = 0;
         uint32_t end = 0;
+        uint32_t startLoop = 0;
+        uint32_t endLoop = 0;
         while(gpioRead(GPIO_ECHO) == 0){
             //printf("[INFO] readGpio: %d\n", gpioRead(GPIO_ECHO));
             //on attend que le signal soit en haut
+            startLoop = time_time();
         }
         start = time_time();
+        printf("difference entre les deux: %f et %f\n", start, startLoop);
         while(gpioRead(GPIO_ECHO) == 1){
             //printf("[INFO] readGpio: %d\n", gpioRead(GPIO_ECHO));
             //on attend que le signal soit en bas
+            endLoop = time_time();
         }
         end = time_time();
+        printf("difference entre les deux: %f et %f\n", end, endLoop);
+
         printf("[INFO] Différence entre les deux: %f   us\n", (end - start));
 
         time_sleep(3);
        
        
-        uint32_t start = gpioTick();
-        time_sleep(3);
-        uint32_t end = gpioTick();
-        printf("[INFO] Différence entre les deux: %zu\n", (end - start));
-       *
-      */
+
+
         
     }
 
