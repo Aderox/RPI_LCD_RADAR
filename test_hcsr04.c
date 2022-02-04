9f9e0f91a08f0aac2d4b6d993cd6e0dc1adb0a08
Aderox <aderoxgame@gmail.com>
2022-02-04 17:43:14 +0100

    working on the actual captor

---
+++ test_hcsr04.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <time.h>
+#include <pigpio.h>
+#include <math.h>
+
+int main(int argc, char *argv[])
+{
+    // branchelent: gnd sur 39 et trig sur 37
+
+    gpioSetMode(37, PI_OUTPUT);
+    gpioServo(37, 1000000);
+
+    time_sleep(1);
+    while (1)
+    {
+        gpioWrite(18, 1); /* on */
+        time_sleep(0.5);
+        gpioWrite(18, 0);
+    }
+}
